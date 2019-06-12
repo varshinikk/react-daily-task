8a5def73459920a1340204434147cf0074130506
varshinkk <varshinishining@gmail.com>
2019-06-12 15:33:54 +0530

    task3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from'react-router-dom'
 import Signup from './Task1/Signup';
 import Lifecycle from './Task2/Lifecycle';
 import User1 from './Task2/User1';
+import Arithmetic from './Task3/Arithmetic';
 
 function App() {
   return (<Router>
@@ -11,6 +12,7 @@ function App() {
     <Route exact path = "/" component = {Signup}></Route>
     <Route exact path = "/Lifecycle" component = {Lifecycle}></Route>
     <Route exact path = "/User1" component = {User1}></Route>
+    <Route exact path = "/Arithmetic" component = {Arithmetic}></Route>
     </switch>
   </Router>
   )
---
+++ src/Task3/Arithmetic.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import './Arithmetic.css'
+
+class Arithmetic extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            num1: 10,
+            num2: 10,
+            num3: 10
+        }
+        this.addAction=this.addAction.bind(this);
+        this.subAction=this.subAction.bind(this);
+        this.mulAction=this.mulAction.bind(this);
+    }
+    
+    addAction(){
+        this.setState({num1:this.state.num1+5});
+    }
+    subAction(){
+        if(this.state.num2>4)
+        {
+            this.setState({num2:this.state.num2-5});
+        }
+    }
+    mulAction(){
+        this.setState({num3:this.state.num3*5});
+    }
+      render(){
+          return(
+              <div>
+                  <div className="div1"><h1 className="heading1">{this.state.num1}</h1>
+                  <button className="button1" onClick={this.addAction}>Addition</button></div>
+                  <div className="div2"><h1 class="heading1">{this.state.num2}</h1>
+                  <button className="button2" onClick={this.subAction}>Subtraction</button></div>
+                  <div className="div3"><h1 class="heading1">{this.state.num3}</h1>
+                  <button className="button3" onClick={this.mulAction}>Multiplication</button></div>
+              </div>
+          );
+      }
+}
+export default Arithmetic;
